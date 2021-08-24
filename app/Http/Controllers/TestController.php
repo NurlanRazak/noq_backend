8aa6e678aa6b42fe3cb78428a52e1a242e0ca70e
NurlanRazak <nurlan.bboy@gmail.com>
2021-08-24 21:10:56 +0600

    parse from prod

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,8 +9,41 @@ class TestController extends Controller
 
     public function test(Request $request)
     {
-        dd('here');
+        $categories = \DB::connection('production')->table('categories')->get();
+        $products = \DB::connection('production')->table('menus')->get();
+        $prod_cats = \DB::connection('production')->table('menu_categories')->get();
 
+        $place = \App\Models\Place::create([
+            'name' => 'Oblaka Bar',
+            'address' => 'Panfilova, 92',
+            'phone' => '+77013995034',
+            'status' => 1,
+        ]);
+
+        foreach ($categories as $category) {
+            $subcategory = \App\Models\Subcategory::create([
+                'category_id' => 8,
+                'name' => $category->name,
+                'status' => 1,
+            ]);
+
+            foreach ($prod_cats as $rel) {
+                if ($rel->category_id == $category->category_id) {
+                    foreach($products as $product) {
+                        if ($product->menu_id == $rel->menu_id) {
+                            \App\Models\Product::create([
+                                'subcategory_id' => $subcategory->id,
+                                'name' => $product->menu_name,
+                                'description' => $product->description,
+                                'price' => floatval($product->menu_price),
+                                'quantity' => $product->stock_qty,
+                                'status' => 1,
+                            ]);
+                        }
+                    }
+                }
+            }
+        }
 
         return view('cloudpayments');
     }
